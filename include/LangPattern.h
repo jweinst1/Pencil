4217e9824276d5f1371e834e38d203cf15ba0487
Joshua Weinstein <jweinstein@splunk.com>
2018-01-06 11:55:43 -0800

    first main file

--- include/LangPattern.h
+++ include/LangPattern.h
@@ -1,4 +1,14 @@
 #ifndef LANG_PATTERN_H
 #define LANG_PATTERN_H
 
+struct LangEntity
+{
+        enum Type
+        {
+                Object,
+                Action,
+                Descript
+        };
+};
+
 #endif
