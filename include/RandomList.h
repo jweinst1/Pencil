acc7673e182b1691d0c9cdc11d3225ba1ef1d5dd
Joshua Weinstein <jweinstein@splunk.com>
2018-01-06 19:07:01 -0800

    file moves

---
+++ include/RandomList.h
@@ -0,0 +1,15 @@
+#ifndef RANDOM_LIST_HEAD
+#define RANDOM_LIST_HEAD
+//Random List Header
+
+// String node class used for random list
+class RandomStr
+{
+public:
+RandomStr(const char* string);
+~RandomStr();
+private:
+char* _str;
+};
+
+#endif
---
+++ src/util/RandomList.h
@@ -0,0 +1,6 @@
+#include "RandomList.h"
+
+RandomStr::RandomStr(const char* string)
+{
+
+}
